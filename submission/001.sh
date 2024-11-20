b9819ed39869d24194fa7abf8561b110cd9d29b3
Edison José de Brito <edisonbrt@gmail.com>
2024-11-20 02:29:46 +0000

    Solução exercicio 001.sh

--- submission/001.sh
+++ submission/001.sh
@@ -1,7 +1,4 @@
 # What is the hash of block 654,321?
-
-ejbrito@fabricio:~/prog/explore-the-blockchain-ejbrito/submission$ bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_069 -rpcpassword=MMrwIpVKXcoW getblockhash 654321
-000000000000000000058452bbe379ad4364fe8fda68c45e299979b492858095
-ejbrito@fabricio:~/prog/explore-the-blockchain-ejbrito/submission$ 
+bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_069 -rpcpassword=MMrwIpVKXcoW getblockhash 654321
 
 
